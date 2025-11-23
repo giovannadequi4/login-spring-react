eed506c6b377a3ff96077478a83fd8bde88a474c
Giovanna <giovanna.dequi@gmail.com>
2025-11-23 19:26:52 -0300

    use argon2

--- backend/src/main/java/com/io/project/configuration/cors/CorsConfiguration.java
+++ backend/src/main/java/com/io/project/configuration/cors/CorsConfiguration.java
@@ -1,4 +1,4 @@
-package com.io.project.configuration;
+package com.io.project.configuration.cors;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
